25e64b0d52c8cfd702cfab7c8fe86b192c847f0b
gleisson.silva <gleisson.silva@grupoamper.com>
2025-07-31 17:42:38 +0100

    First commit

---
+++ src/main/java/com/gtsasil/idempotent_order_api/IdempotentOrderApiApplication.java
@@ -0,0 +1,13 @@
+package com.gtsasil.idempotent_order_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdempotentOrderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdempotentOrderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gtsasil/idempotent_order_api/IdempotentOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtsasil.idempotent_order_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdempotentOrderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
